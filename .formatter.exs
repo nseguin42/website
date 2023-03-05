384160f4bd8f46c2eedf493d73ad77550d0d981e
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-03-04 23:10:01 -0600

    fix: auto format more files
    Signed-off-by: Nick Seguin <5571025+nseguin42@users.noreply.github.com>

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,9 @@
 [
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{ex,exs,heex}", "test/**/*.{ex,exs,heex}", "apps/*/lib/**/*.{ex,exs,heex}"],
+  inputs: [
+    "*.{ex,exs,heex}",
+    "{config,lib,test}/**/*.{ex,exs,heex}",
+    "apps/*/{config,lib,test}/**/*.{ex,exs,heex}"
+  ],
   line_length: 100
 ]
