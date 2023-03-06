1694b8e883b78cb39181c56a50f6924df26413c5
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-03-05 22:51:27 -0600

    refactor: get commit info early and pass it down
    Signed-off-by: Nick Seguin <5571025+nseguin42@users.noreply.github.com>

--- apps/ns_web/lib/ns_web/controllers/page_controller.ex
+++ apps/ns_web/lib/ns_web/controllers/page_controller.ex
@@ -2,8 +2,24 @@ defmodule NsWeb.PageController do
   use NsWeb, :controller
 
   def home(conn, _params) do
+    commit = get_commit()
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home,
+      layout: false,
+      commit_sha: commit["sha"] |> String.slice(0..6),
+      commit_message:
+        commit["commit"]["message"]
+        |> String.split("\n")
+        |> List.first()
+        |> String.slice(0..50)
+        |> String.trim(),
+      commit_url: commit["html_url"],
+      repo_url: "https://github.com/nseguin42/website"
+    )
+  end
+
+  defp get_commit() do
+    Mix.Project.config()[:commit] |> Jason.decode!()
   end
 end
