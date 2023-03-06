4b8295f290430f8ead0314fc8c1d43d0026df4f4
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-03-06 07:16:13 -0600

    fix: temporarily remove commit info
    Signed-off-by: Nick Seguin <5571025+nseguin42@users.noreply.github.com>

--- apps/ns_web/lib/ns_web/controllers/page_controller.ex
+++ apps/ns_web/lib/ns_web/controllers/page_controller.ex
@@ -9,42 +9,4 @@ defmodule NsWeb.PageController do
       repo_url: "https://github.com/nseguin42/website" |> URI.encode()
     )
   end
-
-  defp get_commit() do
-    commit = Mix.Project.config()[:commit]
-
-    if commit == nil do
-      nil
-    else
-      commit |> String.split(" ")
-    end
-  end
-
-  defp get_sha(commit) do
-    if commit == nil do
-      nil
-    else
-      commit["sha"]
-    end
-  end
-
-  defp get_message(commit) do
-    if commit == nil do
-      nil
-    else
-      commit["commit"]["message"]
-      |> String.split("\n")
-      |> List.first()
-      |> String.slice(0..50)
-      |> String.trim()
-    end
-  end
-
-  defp get_url(commit) do
-    if commit == nil do
-      nil
-    else
-      commit["html_url"]
-    end
-  end
 end
